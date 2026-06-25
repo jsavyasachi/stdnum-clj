cf825c406d4f87bd553b83a090c39c4c142edc48
jsavyasachi <25380661+jsavyasachi@users.noreply.github.com>
2026-06-25 05:57:52 +0000

    chore(deps): bump outdated dependencies

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "Eclipse Public License 1.0" :url "https://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/jsavyasachi/stdnum-clj"}
   :dependencies [[commons-validator/commons-validator "1.10.1"]
-                 [org.iban4j/iban4j "3.2.11-RELEASE"]
-                 [org.clojure/data.json "2.5.1"]]
+                 [org.iban4j/iban4j "3.2.13-RELEASE"]
+                 [org.clojure/data.json "2.5.2"]]
   :global-vars {*warn-on-reflection* true}
   :test-selectors {:default (complement :integration)
                    :integration :integration
                    :all (constantly true)}
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.12.0"]]}
-             :dev {:dependencies [[org.clojure/clojure "1.12.0"]]}
-             :clojure-1-10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
-             :clojure-1-11 {:dependencies [[org.clojure/clojure "1.11.4"]]}
-             :clojure-1-12 {:dependencies [[org.clojure/clojure "1.12.0"]]}}
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.12.5"]]}
+             :dev {:dependencies [[org.clojure/clojure "1.12.5"]]}
+             :clojure-1-10 {:dependencies [[org.clojure/clojure "1.12.5"]]}
+             :clojure-1-11 {:dependencies [[org.clojure/clojure "1.12.5"]]}
+             :clojure-1-12 {:dependencies [[org.clojure/clojure "1.12.5"]]}}
   :aliases {"all" ["with-profile" "+clojure-1-10:+clojure-1-11:+clojure-1-12"]}
   :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
                                     :username :env/clojars_username
