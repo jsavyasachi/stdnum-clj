e1bd33d3167c6300877e0783c4781c39a496b285
Savyasachi <jsavyasachi@gmail.com>
2026-07-12 20:20:31 -0700

    chore(release): sync shim project.clj version to 0.30.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.clojars.savya/stdnum-clj "0.30.0"
+(defproject net.clojars.savya/stdnum-clj "0.30.1"
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
